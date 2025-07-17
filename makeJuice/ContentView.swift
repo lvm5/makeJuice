9cf9eb5f0780d554e6c3102ce70a7884a019b88d
Leandro Vansan <59117400+lvm5@users.noreply.github.com>
2025-07-17 10:47:58 -0300

    Initial Commit

---
+++ makeJuice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  makeJuice
+//
+//  Created by Leandro Morais on 2025-07-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ makeJuice/makeJuiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  makeJuiceApp.swift
+//  makeJuice
+//
+//  Created by Leandro Morais on 2025-07-17.
+//
+
+import SwiftUI
+
+@main
+struct makeJuiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
